38d0f628997b728296670f002adea961c7f8b84b
Akhtar <akhtar4660@gmail.com>
2022-01-23 01:39:17 +0530

    Readme created

--- controllers/user.ts
+++ controllers/user.ts
@@ -2,9 +2,13 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
 import { UserType } from "../interfaces/user"
 
 async function addUser(this: FastifyInstance, req: FastifyRequest<{ Body: UserType }>, res: FastifyReply) {
-    let user: UserType = req.body
-    const result = await this.mongo.db?.collection("users").insertOne(user)
-    return res.status(200).send({ status: true, data: { insertId: result?.insertedId } })
+    try {
+        let user: UserType = req.body
+        const result = await this.mongo.db?.collection("users").insertOne(user)
+        return res.status(200).send({ status: true, data: { insertId: result?.insertedId } })
+    } catch (err) {
+        return res.status(500).send({ status: false, message: "Server error" })
+    }
 }
 
 const UserController = {
